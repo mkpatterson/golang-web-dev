ee10a2de5290331e4fcddcfc2a5a66444c167f14
mkpatterson <mkp@dataspoke.tech>
2018-05-11 11:18:48 -0400

    templates

--- 002_template/01_stdout/main.go
+++ 002_template/01_stdout/main.go
@@ -1,1 +1,22 @@
-package _1_stdout
+package main
+
+import "fmt"
+
+func main() {
+	name := "Terry Flaps"
+
+	tpl := `
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	<meta charset="UTF-8">
+	<title>Hello World!</title>
+	</head>
+	<body>
+	<h1>` + name + `<h1>
+	</body>
+	</html>
+	`
+
+	fmt.Println(tpl)
+}
